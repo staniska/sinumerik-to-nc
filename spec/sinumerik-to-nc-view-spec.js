4de626300c573f9665fd16ce606b9d7431a91e69
a3boot <a3boot.babaev@gmail.com>
2021-06-15 22:28:10 +0300

    Init

---
+++ spec/sinumerik-to-nc-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SinumerikToNcView from '../lib/sinumerik-to-nc-view';
+
+describe('SinumerikToNcView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
